9e3fb68e06c289fd089dcb2f1a25229bfefac720
Andrey Markelov <andrey.v.markelov@gmail.com>
2013-09-23 16:33:15 +0400

    refac

--- src/main/java/ru/mail/jira/plugins/UniqueRegexConfig.java
+++ src/main/java/ru/mail/jira/plugins/UniqueRegexConfig.java
@@ -1,7 +1,3 @@
-/*
- * Created by Andrey Markelov 28-10-2012.
- * Copyright Mail.Ru Group 2012. All rights reserved.
- */
 package ru.mail.jira.plugins;
 
 import java.util.ArrayList;
@@ -13,45 +9,21 @@ import com.atlassian.jira.security.Permissions;
 import com.atlassian.jira.web.action.JiraWebActionSupport;
 import com.atlassian.sal.api.ApplicationProperties;
 
-/**
- * Configuration action.
- * 
- * @author Andrey Markelov
- */
-public class UniqueRegexConfig
-    extends JiraWebActionSupport
-{
-    /**
-     * Unique ID.
-     */
+public class UniqueRegexConfig extends JiraWebActionSupport {
     private static final long serialVersionUID = 1381842671050861762L;
 
-    /**
-     * Application properties.
-     */
     private final ApplicationProperties applicationProperties;
 
-    /**
-     * Datas.
-     */
     private List<CFData> datas;
+    private String customFieldId;
 
-    /**
-     * Constructor.
-     */
-    public UniqueRegexConfig(
-        UniqueRegexMgr urMgr,
-        ApplicationProperties applicationProperties,
-        CustomFieldManager cfMgr)
-    {
+    public UniqueRegexConfig(UniqueRegexMgr urMgr, ApplicationProperties applicationProperties, CustomFieldManager cfMgr) {
         this.applicationProperties = applicationProperties;
         this.datas = new ArrayList<CFData>();
 
         List<CustomField> cgList = cfMgr.getCustomFieldObjects();
-        for (CustomField cf : cgList)
-        {
-            if (cf.getCustomFieldType().getKey().equals("ru.mail.jira.plugins.uniqueregexfield:unique-regex-cf"))
-            {
+        for (CustomField cf : cgList) {
+            if (cf.getCustomFieldType().getKey().equals("ru.mail.jira.plugins.uniqueregexfield:unique-regex-cf")) {
                 CFData cFData = urMgr.getCFData(cf.getId());
                 cFData.setCfKey(cf.getId());
                 cFData.setCfName(cf.getName());
@@ -60,35 +32,64 @@ public class UniqueRegexConfig
         }
     }
 
-    /**
-     * Get context path.
-     */
-    public String getBaseUrl()
-    {
+    @Override
+    public String doDefault() throws Exception {
+        if (!hasAdminPermission()) {
+            return PERMISSION_VIOLATION_RESULT;
+        }
+        return INPUT;
+    }
+
+    public String doConfigure() throws Exception {
+        if (!hasAdminPermission()) {
+            return PERMISSION_VIOLATION_RESULT;
+        }
+
+        if (customFieldId == null || customFieldId.length() == 0) {
+            return INPUT;
+        } else {
+            
+            return "configure";
+        }
+    }
+
+    @Override
+    @com.atlassian.jira.security.xsrf.RequiresXsrfCheck
+    protected String doExecute() throws Exception {
+        return getRedirect("UniqueRegexConfig!default.jspa?saved=true");
+    }
+
+    @Override
+    protected void doValidation() {
+        super.doValidation();
+    }
+
+    public String getBaseUrl() {
         return applicationProperties.getBaseUrl();
     }
 
-    public List<CFData> getDatas()
-    {
+    public String getCustomFieldId() {
+        return customFieldId;
+    }
+
+    public List<CFData> getDatas() {
         return datas;
     }
 
-    /**
-     * Check administer permissions.
-     */
-    public boolean hasAdminPermission()
-    {
+    public boolean hasAdminPermission() {
         User user = getLoggedInUser();
-        if (user == null)
-        {
+        if (user == null) {
             return false;
         }
 
-        if (getPermissionManager().hasPermission(Permissions.ADMINISTER, getLoggedInUser()))
-        {
+        if (getPermissionManager().hasPermission(Permissions.ADMINISTER, getLoggedInUser())) {
             return true;
         }
 
         return false;
+    }
+
+    public void setCustomFieldId(String customFieldId) {
+        this.customFieldId = customFieldId;
     }
 }
